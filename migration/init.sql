eadcfe2daf7e4829800a10464f57ea18bf936d2c
romik1505 <romik1505@yandex.ru>
2021-08-11 08:56:55 +0300

    Docker migrate

--- migration/init.sql
+++ migration/init.sql
@@ -2,11 +2,11 @@ CREATE DATABASE urlsshortener;
 
 \connect urlsshortener;
 
-CREATE TABLE urlsshortener.urls
+CREATE TABLE urls
 (
     id        SERIAL PRIMARY KEY,
     original  VARCHAR(255) UNIQUE,
     shortener VARCHAR(10) UNIQUE
 );
 
-INSERT INTO urlsshortener.urls (original, shortener) VALUES ('TEST','TEST');
+INSERT INTO urls (original, shortener) VALUES ('TEST','TEST');
